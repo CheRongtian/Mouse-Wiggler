2cc0a626ecf27e296ad5703c7b132ce0b1754a68
CheRongtian <cherongtian0702@gmail.com>
2026-02-11 17:42:14 +0000

    finish the code, but README.md

--- mouse_wiggler.c
+++ mouse_wiggler.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <libevdev/libevdev.h>
 #include <libevdev/libevdev-uinput.h>
+#include <unistd.h>
+#include <time.h>
 
 int main()
 {
@@ -9,14 +11,35 @@ int main()
 	struct libevdev *dev = libevdev_new();
 	struct libevdev_uinput *uidev = NULL;
 
+	libevdev_set_name(dev, "USB Fake Mouse");
+	// Define what the is mouse if capable of
+	libevdev_enable_event_type(dev, EV_REL);
+	libevdev_enable_event_code(dev, EV_REL, REL_X, NULL);
+	libevdev_enable_event_code(dev, EV_REL, REL_Y, NULL);
+
+	libevdev_enable_event_type(dev, EV_KEY);
+	libevdev_enable_event_code(dev, EV_KEY, BTN_LEFT, NULL);
+
+	libevdev_enable_event_type(dev, EV_SYN);
+	libevdev_enable_event_code(dev, EV_SYN, SYN_REPORT, NULL);
+
 	int code = libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED, &uidev);
 	if(code != 0) 
 	{
 		perror("Error");
 		return -1;
 	}
-	// Define what the is mouse if capable of
+
+	srand(time(NULL));
 	// Fire fake mouse events
+	while(1)
+	{
+		int amplitude = rand() % 410 - 200; // random number between [-20, 20]
+		int direction = rand() % 2 == 0 ? REL_X : REL_Y; 
+		libevdev_uinput_write_event(uidev, EV_REL, direction, amplitude);
+		libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+		usleep(500000);
+	}
 	// printf("Hello World\n"); // just for test head files
 	return 0;
 }
